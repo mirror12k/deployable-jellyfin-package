2aadc4c93bb698b032b23d5f77aac2de76509991
mirror12k <mirror12k@gmail.com>
2025-09-21 10:21:18 -0400

    - jellyfin

---
+++ jellyfin/run.sh
@@ -0,0 +1,6 @@
+
+mkdir -p /jellyfin/config
+mkdir -p /jellyfin/cache
+mkdir -p /jellyfin/media
+
+docker-compose up -d
---
+++ run-package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+user=$1
+container_name="$2"
+package_name="${3:-$2}"
+
+git pull
+
+# run docker cli within a limited environment so that the container can initialize itself safely
+docker run --privileged --name "$container_name-container" --rm \
+    -v "$(pwd)/$package_name:/app:ro" \
+    -v "/$container_name:/$container_name" \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -w "/app" -it docker:cli "sh" "/app/run.sh"
