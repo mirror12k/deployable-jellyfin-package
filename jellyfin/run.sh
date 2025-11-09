8c75e08439b4060cc2d2bbdc2b721a07133a4ca7
Mirror <mirror12k@gmail.com>
2025-11-09 11:44:05 -0500

    - updates

--- jellyfin/run.sh
+++ jellyfin/run.sh
@@ -1,6 +1,23 @@
+#!/bin/bash
+set -e
 
-mkdir -p /jellyfin/config
-mkdir -p /jellyfin/cache
-mkdir -p /jellyfin/media
+JELLYFIN_ROOT="/jellyfin"
 
+echo "[i] cleaning up old data..."
+rm -rf "$JELLYFIN_ROOT/config"
+rm -rf "$JELLYFIN_ROOT/cache"
+
+echo "[i] creating jellyfin directories..."
+mkdir -p "$JELLYFIN_ROOT/config"
+mkdir -p "$JELLYFIN_ROOT/cache"
+mkdir -p "$JELLYFIN_ROOT/media"
+
+echo "[i] copying docker-compose to jellyfin root..."
+cp /app/docker-compose.yml "$JELLYFIN_ROOT/docker-compose.yml"
+
+cd "$JELLYFIN_ROOT"
+
+echo "[i] pulling images"
+docker-compose pull
+echo "[i] starting jellyfin docker..."
 docker-compose up
